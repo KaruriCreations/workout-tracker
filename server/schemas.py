005034e83b56e845f0af0dfeec4a8dc97681140a
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-07-25 15:46:28 +0300

    created exerciese Schema

--- server/schemas.py
+++ server/schemas.py
@@ -2,3 +2,11 @@
 from marshmallow import Schema, fields, validate
 from models import Exercise, Workout, WorkoutExercise
 
+class ExerciseSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.Str(required=True)
+    category = fields.Str(required=True)
+    equipment_needed = fields.Boolean()
+    
+    
+
