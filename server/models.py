c908fd79f549e230d742bf12710f168473998591
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-07-25 13:58:10 +0300

    created exerciese model

--- server/models.py
+++ server/models.py
@@ -4,3 +4,15 @@ from sqlalchemy.orm import validates
 
 # Initialize SQLAlchemy instance
 db = SQLAlchemy()
+
+#exercise model
+class Exercise(db.model):
+    __tablename__ = "exercises"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.String, nullable=False)
+
+    
+    
