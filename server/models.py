fd3455f0caf7cdca0610ed0c3c57b2f65c88083d
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-07-25 13:55:20 +0300

    imported sql alchemy

--- server/models.py
+++ server/models.py
@@ -1,1 +1,6 @@
 # Defines DB tables (Exercise, Workout, WorkoutExercise) + @validates
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+
+# Initialize SQLAlchemy instance
+db = SQLAlchemy()
