2c496e6b6c1fa7ae15f0812d212b6c609ec564e7
KaruriCreations <266662126+KaruriCreations@users.noreply.github.com>
2026-07-26 16:49:36 +0300

    imported everything needed for my routes

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
 # Configures Flask app, DB, and API routes (/workouts, /exercises)
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from datetime import datetime
+
+# Import database, models, and schemas
+from models import db, Exercise, Workout, WorkoutExercise
+from schemas import (
+    workout_schema, workouts_schema,
+    exercise_schema, exercises_schema,
+    workout_exercise_schema
+)
